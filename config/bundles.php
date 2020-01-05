a39589a133fb57f6c2dead0acae6ef5859afe7c4
Jean-Yves CAMIER <jycamier@clever-age.com>
2020-01-05 15:30:32 +0100

    Add deps for boilerplate

--- config/bundles.php
+++ config/bundles.php
@@ -1,5 +1,12 @@
-<?php
+in<?php
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Sidus\BaseBundle\SidusBaseBundle::class => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    CleverAge\ProcessBundle\CleverAgeProcessBundle::class => ['all' => true],
+    Jycamier\MessengerProcessBundle\JycamierMessengerProcessBundle::class => ['all' => true],
+    Nelmio\Alice\Bridge\Symfony\NelmioAliceBundle::class => ['dev' => true, 'test' => true],
+    Jycamier\AliceProcessBundle\JycamierAliceProcessBundle::class => ['all' => true],
+    Jycamier\ConsoleCommandProcessBundle\ConsoleCommandProcessBundle::class => ['all' => true],
 ];
